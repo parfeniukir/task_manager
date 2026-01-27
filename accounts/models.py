fc8176c9c1bc219c8e361c4fd70d1924f1c5cea6
Ivanna Parfeniuk <parfeniuk.ivannaa@gmail.com>
2026-01-27 21:15:10 +0200

    Add acounts app. CustomUser, login, sighup pages

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(
+        upload_to="avatars/",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.username
